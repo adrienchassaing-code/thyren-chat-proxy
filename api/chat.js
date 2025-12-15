1df781260d902878da7b441c9a52736f7993d9ae
adrienchassaing-code <adrien.chassaing@suplemint.com>
2025-12-15 13:17:53 +0100

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,6 +1,5 @@
 import fs from "fs";
 import path from "path";
-import { redis } from "../lib/redis.js";
 
 // ====== Lecture des fichiers DATA depuis /data ======
 const readDataFile = (filename) => {
