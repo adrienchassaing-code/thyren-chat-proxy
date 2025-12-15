cfdd97c13893c2289c3739647e41488f20d976b7
adrienchassaing-code <adrien.chassaing@suplemint.com>
2025-12-15 12:08:09 +0100

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,11 +1,5 @@
 import fs from "fs";
 import path from "path";
-import { Redis } from "@upstash/redis";
-
-const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
-});
 
 // ====== Lecture des fichiers DATA depuis /data ======
 const readDataFile = (filename) => {
