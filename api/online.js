d791632f5dcb7be456c53c0bd8c1ad789dc5f469
adrienchassaing-code <adrien.chassaing@suplemint.com>
2025-12-16 10:32:59 +0100

    Update online.js

--- api/online.js
+++ api/online.js
@@ -1,3 +1,26 @@
+function setCors(req, res) {
+  const origin = req.headers.origin || "*";
+
+  // Autorise ton site + preview + autres (safe)
+  const allowed = [
+    "https://www.suplemint.com",
+    "https://suplemint.com",
+  ];
+
+  // Si c'est ton domaine => on renvoie le vrai origin
+  if (allowed.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  } else {
+    // fallback pour tests / dev (tu peux aussi mettre "https://www.suplemint.com" à la place)
+    res.setHeader("Access-Control-Allow-Origin", "*");
+  }
+
+  res.setHeader("Vary", "Origin");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Max-Age", "86400");
+}
+
 // api/online.js
 module.exports = async function handler(req, res) {
   // CORS
